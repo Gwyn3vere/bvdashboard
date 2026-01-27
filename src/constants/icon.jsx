be75e0dc6987930b809840ff917449ba1535ac31
Gwyn3vere <tangphuc85@gmail.com>
2026-01-27 18:59:40 +0700

    ui(expertise): create view mode

--- src/constants/icon.jsx
+++ src/constants/icon.jsx
@@ -1,7 +1,58 @@
-import { LuBuilding2, LuTestTubeDiagonal, LuZap } from "react-icons/lu";
+import {
+  LuBuilding2,
+  LuTestTubeDiagonal,
+  LuBriefcaseMedical,
+  LuBuilding,
+  LuNotepadText,
+  LuCircleDollarSign,
+  LuStethoscope,
+  LuHeart,
+  LuScissors,
+  LuBaby,
+  LuClipboardPlus,
+  LuSiren,
+  LuSyringe,
+  LuToyBrick,
+  LuBug,
+  LuLeaf,
+  LuEye,
+  LuMicroscope,
+  LuFlaskConical,
+  LuShield,
+  LuPill,
+  LuHeartPulse
+} from "react-icons/lu";
 
-export const GROUP_ICON_MAP = {
-  PHONG_CHUC_NANG: LuBuilding2,
-  LAM_SANG: LuTestTubeDiagonal,
-  CAN_LAM_SANG: LuZap
+export const ICON_MAP = {
+  // Group/Block icons
+  "building-corporate": LuBuilding2,
+  "medical-clinic": LuBriefcaseMedical,
+  laboratory: LuTestTubeDiagonal,
+
+  // Department icons - Khối phòng chức năng
+  planning: LuBuilding,
+  admin: LuNotepadText,
+  finance: LuCircleDollarSign,
+  nursing: LuHeartPulse,
+
+  // Department icons - Khối lâm sàng
+  "internal-medicine": LuHeart,
+  surgery: LuScissors,
+  obstetrics: LuBaby,
+  outpatient: LuClipboardPlus,
+  emergency: LuSiren,
+  anesthesia: LuSyringe,
+  pediatrics: LuToyBrick,
+  infectious: LuBug,
+  traditional: LuLeaf,
+  "dental-ent": LuEye,
+
+  // Department icons - Khối cận lâm sàng
+  imaging: LuMicroscope,
+  "lab-test": LuFlaskConical,
+  "infection-control": LuShield,
+  pharmacy: LuPill,
+
+  // Default
+  stethoscope: LuStethoscope
 };
