32636bce7ca836fb55d976f93bf9dea9c7ad60ed
Gwyn3vere <tangphuc85@gmail.com>
2026-02-04 22:25:23 +0700

    refactor(header): refactor dropdown

---
+++ src/constants/menu.jsx
@@ -0,0 +1,6 @@
+import { LuUser, LuSettings, LuLogOut } from "react-icons/lu";
+
+export const NAV_MENU = [
+  { id: "account", icon: <LuUser />, value: "ACCOUNT", name: "Tài khoản" },
+  { id: "setting", icon: <LuSettings />, value: "SETTING", name: "Cài đặt" }
+];
